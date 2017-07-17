81ef0b9aab4ea06ee5da0436205aa364828f5422
Nicholas Wei <ncwei@uci.edu>
2017-07-17 16:32:36 -0700

    Initial commit

---
+++ BitWrapper.swift
@@ -0,0 +1,255 @@
+//
+//  BitWrapper.swift
+//  PWB
+//
+//  Created by Nicholas Wei on 7/12/17.
+//  Copyright © 2017 Nicholas Wei. All rights reserved.
+//
+//  TODO:
+//  - turn flip/constFlip into single function
+//  - cleanup viewing (put public functions/properties above internal implementation details)
+
+import Foundation
+
+
+enum BitWrapperError: Error
+{
+    case invalidInit(reason: String)
+    case badArgs
+}
+
+class BitWrapper    // class to represent binary numbers for easy manipulations in the game
+{
+    
+    // private var fullBinary: String?    // contains the entire binary number in string form; NOT UPDATED IN REAL TIME/WILL BE EDITED OUT LATER
+    private var binary: [Character]!         // binary number stored in array with each slot representing a char-form bit of fullBinary
+    
+    var expansion: [Character]
+    {
+        return binary
+    }
+    
+    init(fromString stringNum: String, alreadyBinary: Bool) // initializes from a string of an int
+    {
+        if let number = Int(stringNum)  // ensure that number is valid regardless if its in binary form or not
+        {
+            if !alreadyBinary
+            {
+                binary = Array(String(number, radix: 2).characters) // convert to binary
+            }
+            else
+            {
+                binary = Array(stringNum.characters)
+            }
+            // removeLeadingZeros()
+        }
+        //        else    // bad arguments
+        //        {
+        //            throw BitWrapperError.invalidInit(reason: "String does not represent an int")
+        //        }
+    }
+    
+    init(fromNum number: Int) // initializes from an Int
+    {
+        binary = Array(String(number, radix: 2).characters)
+    }
+    
+    init(fromBit wrapper: BitWrapper)   // initialize from another BitWrapper
+    {
+        binary = wrapper.binary
+        // removeLeadingZeros()
+    }
+    
+    func removeLeadingZeros()   // ex. "00010" to "10"
+    {
+        if (binary.count > 1 && binary?[0] == "0")
+        {
+            binary.removeFirst()
+            removeLeadingZeros()
+        }
+    }
+    
+    func addLeadingZeros(num: Int)
+    {
+        binary = Array(String(repeating: "0", count: num).characters) + binary
+    }
+    
+    private func constFlip(index: Int)  // flips the bit solely and returns the previous bit
+    {
+        if let bit = binary?[index]
+        {
+            if bit == "0"
+            {
+                binary[index] = "1"
+            }
+            else
+            {
+                binary[index] = "0"
+            }
+        }
+        
+    }
+    
+    func flip(index: Int) // flips the bit and removes leading 0s if needed
+    {
+        constFlip(index: index)
+        // removeLeadingZeros()
+    }
+    
+    func shiftLeft()
+    {
+        binary.removeFirst()
+        binary.append("0")
+        // removeLeadingZeros()
+    }
+    
+    func shiftRight()
+    {
+        if binary.count > 1
+        {
+            binary.removeLast()
+        }
+        else
+        {
+            binary[0] = "0"
+        }
+        
+        binary!.insert("0", at: 0)
+    }
+    
+    func toStr() -> String
+    {
+        return String(binary!)
+    }
+    
+    func toInt() -> Int
+    {
+        return Int(self.toStr(), radix: 2)!
+    }
+    
+    static func selfAdd(fromBit: BitWrapper) -> BitWrapper   // adds itself together as if each bit was its own number ex. selfAdd("11110") = 4 or 0b100
+    {
+        return BitWrapper(fromNum: fromBit.binary.filter{$0 == "1"}.count)
+    }
+    
+    static func selfAnd(fromBit: BitWrapper) -> BitWrapper   // ANDs itself together as if each bit was independent
+    {
+        if (fromBit.binary.filter{$0 == "0"}.count) >= 1
+        {
+            return BitWrapper(fromNum: 0)
+        }
+        return BitWrapper(fromNum: 1)
+    }
+    
+    static func selfOr(fromBit: BitWrapper) -> BitWrapper    // ORs the bit as if each bit was independent
+    {
+        if (fromBit.binary.filter{$0 == "1"}.count) >= 1
+        {
+            return BitWrapper(fromNum: 1)
+        }
+        return BitWrapper(fromNum: 0)
+    }
+    
+    func selfNot()   // NOTs the bits modifies original self
+    {
+        
+        for i in 0..<binary.count
+        {
+            constFlip(index: i)
+        }
+        // removeLeadingZeros()
+    }
+    
+    static func add(lhs: BitWrapper, rhs: BitWrapper) -> BitWrapper    // static bitwise addition function, does not require constructed BitWrapper
+    {
+        return BitWrapper(fromNum: (lhs.toInt() + rhs.toInt()))
+    }
+    
+    
+    static func or(lhs: BitWrapper, rhs: BitWrapper) -> BitWrapper  // static OR bitwise function
+    {
+        if lhs.binary.count == rhs.binary.count
+        {
+            var stringBit = ""
+            
+            for i in 0..<lhs.binary.count
+            {
+                if lhs.binary[i] == "1" || rhs.binary[i] == "1"
+                {
+                    stringBit += "1"
+                }
+                else
+                {
+                    stringBit += "0"
+                }
+            }
+            
+            return BitWrapper(fromString: stringBit, alreadyBinary: true)
+        }
+        else
+        {
+            return BitWrapper(fromNum: -1)
+        }
+    }
+    
+    static func and(lhs: BitWrapper, rhs: BitWrapper) -> BitWrapper
+    {
+        if lhs.binary.count == rhs.binary.count
+        {
+            var stringBit = ""
+            
+            for i in 0..<lhs.binary.count
+            {
+                if lhs.binary[i] == "1" && rhs.binary[i] == "1"
+                {
+                    stringBit += "1"
+                }
+                else
+                {
+                    stringBit += "0"
+                }
+            }
+            
+            return BitWrapper(fromString: stringBit, alreadyBinary: true)
+        }
+        else
+        {
+            return BitWrapper(fromNum: -1)  // don't want to throw error, instead just return -1 in BitWrapper form since all levels deal with only positive nums
+        }
+    }
+    
+    static func xor(lhs: BitWrapper, rhs: BitWrapper) -> BitWrapper
+    {
+        if lhs.binary.count == rhs.binary.count
+        {
+            var stringBit = ""
+            
+            for i in 0..<lhs.binary.count
+            {
+                if lhs.binary[i] == "1" && rhs.binary[i] == "1"
+                {
+                    stringBit += "0"
+                }
+                else if lhs.binary[i] == "1" || rhs.binary[i] == "1"
+                {
+                    stringBit += "1"
+                }
+                else
+                {
+                    stringBit += "0"
+                }
+            }
+            
+            return BitWrapper(fromString: stringBit, alreadyBinary: true)
+        }
+        else
+        {
+            return BitWrapper(fromNum: -1)
+        }
+    }
+    
+    static func equals(lhs: BitWrapper, rhs: BitWrapper) -> Bool    // compares two BitWrappers for equality, leading zeros are not factored into equality, only their pure numerical value
+    {
+        return lhs.toInt() == rhs.toInt()
+    }
+} // class
---
+++ PWBLevel.swift
@@ -0,0 +1,332 @@
+//
+//  PWBLevel.swift
+//  PWB
+//
+//  Created by Nicholas Wei on 7/13/17.
+//  Copyright © 2017 Nicholas Wei. All rights reserved.
+//
+//  TODO:
+//  - test top, left, and right orientation to ensure operations/answers work
+//  - test bottom orientation xor func; rest confirmed to work
+//  - cleanup viewing, some var names too...
+//  - implement move restrictions(?)
+
+import Foundation
+
+enum PWBLevelError: Error
+{
+    case invalidInit(reason: String)
+}
+
+typealias EndState = Dictionary<String, BitWrapper> // Dictionary must use the keywords: "top", "bot", "left", "right"
+typealias DirectionOfOperations = Dictionary<String, String> // Dictionary will map <direction, operation-name> ex. <"up", "and">, <"down", "or">; indicates the operation to be performed
+typealias BinaryOperation = (BitWrapper, BitWrapper) -> BitWrapper // operations that operate on the up/down orientation; they deal with rows of bitwrappers hence binary
+typealias UnaryOperation = (BitWrapper) -> BitWrapper   // operations that operate on the right/left orientation; they deal with singular bitwrappers hence unary
+
+class PWBLevel  // represents a "level" in the game, utilizes BitWrapper
+{
+    private let name: String!
+    private let startState: [BitWrapper]!
+    private var currentState: [BitWrapper]!
+    private var finishState: DesiredState!
+    private var opDirections: OperationOrientation!
+    private var gameState: GameState?
+    private var active = false // whether or not the game has started, once set to true, some functions are allowed/restricted
+    
+    var levelName: String
+    {
+        return name
+    }
+    
+    var state: GameState?
+    {
+        if let levelState = gameState
+        {
+            return levelState
+        }
+        return nil
+    }
+    
+    var inGame: Bool
+    {
+        return active
+    }
+    
+    init(withName: String, startFrom: [BitWrapper], endWith: EndState, withOperations: DirectionOfOperations) throws
+    {
+        name = withName
+        startState = startFrom
+        currentState = startState
+        guard startFrom.count > 1 else {
+            throw PWBLevelError.invalidInit(reason: "Must start with at least two BitWrappers in startFrom array")
+        }
+        guard readDesiredState(endState: endWith) != endWith.count else {
+            throw PWBLevelError.invalidInit(reason: "Bad EndState")
+        }
+        guard readOperationOrientation(withOps: withOperations) != withOperations.count else {
+            throw PWBLevelError.invalidInit(reason: "Bad Operation Orientations")
+        }
+    }
+    
+    struct GameState    // represents the current state of the game with additional info
+    {
+        let levelName: String
+        let startingState: [BitWrapper]
+        let goal: DesiredState
+        
+        var currentState: [BitWrapper]
+        var stateSize: Int    // size of the currentState
+        var moves: Int  // total number of "moves" performed
+        var completed: Bool
+        
+        // let manipulationResult: AnyObject?  // result of any operations performed as a result of the last performManipulation() if applicable
+        
+    }
+    
+    struct DesiredState // represents the goal to the problem, all bits are represented as BitWrappers, there are 8 desired states possible (minimum one required)
+    {
+        var topState: BitWrapper?
+        var bottomState: BitWrapper?
+        var rightState: BitWrapper?
+        var leftState: BitWrapper?
+        
+        // var topRightState: BitWrapper?
+        // var bottomRightState: BitWrapper?
+        
+        // var topLeftState: BitWrapper?
+        // var bottomLeftState: BitWrapper?
+        
+    }
+    
+    private struct OperationOrientation
+    {
+        var up: BinaryOperation?
+        var down: BinaryOperation?
+        var right: UnaryOperation?
+        var left: UnaryOperation?
+    }
+    
+    private func readDesiredState(endState: EndState) -> Int // returns number of "bad" entries in the dictionary
+    {
+        finishState = DesiredState()
+        var faults = 0
+        for (orientation, state) in endState
+        {
+            switch orientation
+            {
+            case "top": finishState.topState = state
+            case "bot": finishState.bottomState = state
+            case "right": finishState.rightState = state
+            case "left": finishState.leftState = state
+                //            case "top-right": finishState.topRightState = state
+                //            case "top-left": finishState.topLeftState = state
+                //            case "bottom-right": finishState.bottomRightState = state
+            //            case "bottom-left": finishState.bottomLeftState = state
+            default : faults += 1
+            }
+        }
+        return faults
+    }
+    
+    private func readOperationOrientation(withOps: DirectionOfOperations) -> Int // returns number of bad entries in dictionary
+    {
+        opDirections = OperationOrientation()
+        var faults = 0
+        for (orientation, operation) in withOps
+        {
+            if orientation == "up" || orientation == "down"
+            {
+                faults += assignBinaryOperation(withOp: operation, withDir: orientation)
+            }
+            else if orientation == "right" || orientation == "left"
+            {
+                faults += assignUnaryOperation(withOp: operation, withDir: orientation)
+            }
+            else
+            {
+                faults += 1
+            }
+        }
+        return faults
+    }
+    
+    private func assignBinaryOperation(withOp: String, withDir: String) -> Int // returns 1 if fault detected, else 0
+    {
+        if withDir == "up"
+        {
+            switch withOp
+            {
+            case "add" : opDirections.up = BitWrapper.add
+            case "or" : opDirections.up = BitWrapper.or
+            case "and" : opDirections.up = BitWrapper.and
+            case "xor" : opDirections.up = BitWrapper.xor
+            default : return 1
+            }
+        }
+        else
+        {
+            switch withOp
+            {
+            case "add" : opDirections.down = BitWrapper.add
+            case "or" : opDirections.down = BitWrapper.or
+            case "and" : opDirections.down = BitWrapper.and
+            case "xor" : opDirections.down = BitWrapper.xor
+            default : return 1
+            }
+        }
+        return 0
+    }
+    
+    private func assignUnaryOperation(withOp: String, withDir: String) -> Int
+    {
+        if withDir == "right"
+        {
+            switch withOp
+            {
+            case "add" : opDirections.right = BitWrapper.selfAdd
+            case "or" : opDirections.right = BitWrapper.selfOr
+            case "and" : opDirections.right = BitWrapper.selfAnd
+            default : return 1
+            }
+        }
+        else
+        {
+            switch withOp
+            {
+            case "add" : opDirections.left = BitWrapper.selfAdd
+            case "or" : opDirections.left = BitWrapper.selfOr
+            case "and" : opDirections.left = BitWrapper.selfAnd
+            default : return 1
+            }
+        }
+        return 0
+    }
+    
+    func start()    // sets up the game state and allows calls to performManipulation, can also be used to restart the level
+    {
+        gameState = GameState(levelName: name, startingState: startState, goal: finishState,
+                              currentState: currentState, stateSize: currentState.count, moves: 0, completed: false)
+        active = true
+    }
+    
+    func action(rowIndex: Int, action: String, numIndex: Int? = nil) -> GameState? // perform action and update/return game state
+    {
+        if active
+        {
+            let move = performManipulation(index: rowIndex, manipulation: action, withExtraParam: numIndex)
+            updateGameState(moves: move)
+        }
+        return state
+        
+    }
+    
+    private func updateGameState(moves: Int)
+    {
+        gameState!.currentState = currentState
+        gameState!.stateSize = currentState.count
+        gameState!.moves += moves
+        gameState!.completed = checkCompletion()
+        if gameState!.completed
+        {
+            active = false
+        }
+        
+    }
+    
+    private func performManipulation(index: Int, manipulation: String, withExtraParam: Int? = nil) -> Int // handles flip, not, and shifts; returns 1 if successful, 0 if not
+    {
+        switch manipulation
+        {
+        case "shift-left" : currentState[index].shiftLeft()
+        case "shift-right" : currentState[index].shiftRight()
+        case "not" : currentState[index].selfNot()
+        case "flip" : if let binaryIndex = withExtraParam {currentState[index].flip(index: binaryIndex)} else {return 0}
+        default : return 0
+        }
+        return 1
+    }
+    
+    private func checkCompletion() -> Bool
+    {
+        if finishState.topState != nil
+        {
+            if !checkDirection(direction: "up")
+            {
+                return false
+            }
+        }
+        if finishState.bottomState != nil
+        {
+            if !checkDirection(direction: "down")
+            {
+                return false
+            }
+        }
+        if finishState.rightState != nil
+        {
+            if !checkDirection(direction: "right")
+            {
+                return false
+            }
+        }
+        if finishState.leftState != nil
+        {
+            if !checkDirection(direction: "left")
+            {
+                return false
+            }
+        }
+        return true // passed all the checks, all states will never be nil at the same time due to checks at init
+    }
+    
+    private func checkDirection(direction: String) -> Bool
+    {
+        var equals = false
+        var bitToCheck: BitWrapper?
+        var unaryFunc: UnaryOperation?
+        var binaryFunc: BinaryOperation?
+        var binary: Bool
+        
+        switch direction
+        {
+        case "up" : bitToCheck = finishState.topState; binaryFunc = opDirections.up; binary = true
+        case "down" : bitToCheck = finishState.bottomState; binaryFunc = opDirections.down; binary = true
+        case "right" : bitToCheck = finishState.rightState; unaryFunc = opDirections.right; binary = false
+        case "left" : bitToCheck = finishState.leftState; unaryFunc = opDirections.left; binary = false
+        default : return false
+        }
+        
+        if let answer = bitToCheck
+        {
+            if binary
+            {
+                if let operation = binaryFunc
+                {
+                    let initialValue = currentState[0]
+                    let computedValue = currentState[1..<currentState.count].reduce(initialValue, operation)
+                    
+                    // print(computedValue.toStr())
+                    equals = BitWrapper.equals(lhs: answer, rhs: computedValue)
+                }
+            }
+            else
+            {
+                if let operation = unaryFunc
+                {
+                    var strAnswer = ""
+                    for bit in currentState
+                    {
+                        strAnswer += operation(bit).toStr()
+                    }
+                    
+                    let computedValue = BitWrapper(fromString: strAnswer, alreadyBinary: true)
+                    
+                    equals = BitWrapper.equals(lhs: answer, rhs: computedValue)
+                }
+            }
+        }
+        
+        return equals
+    }
+    
+} // class
