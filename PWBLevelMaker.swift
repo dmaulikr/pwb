bd4efd82dd5aaca38a54e58f8c9603b7c7f26faf
Nicholas Wei <ncwei@uci.edu>
2017-07-21 22:50:58 -0700

    Added more test levels

--- PWBLevelMaker.swift
+++ PWBLevelMaker.swift
@@ -73,3 +73,35 @@ func constructTestLevelWithThreeBits() -> PWBLevel?
     }
     return game
 }
+
+func constructTestLevelSmall() -> PWBLevel?
+{
+    var game: PWBLevel?
+    
+    let goal: EndState = [
+        "bot" : BitWrapper(fromString: "1010", alreadyBinary: true),
+        "right" : BitWrapper(fromString: "10", alreadyBinary: true),
+        "top" : BitWrapper(fromString: "1110", alreadyBinary: true),
+        "left" : BitWrapper(fromString: "01", alreadyBinary: true)
+    ]
+    
+    let ops: DirectionOfOperations = [
+        "down" : "add"
+    ]
+    
+    let name = "PWB_Test_Level_1"
+    
+    let startFrom: [BitWrapper] = [
+        BitWrapper(fromString: "1000", alreadyBinary: true),
+        BitWrapper(fromString: "0100", alreadyBinary: true)
+    ]
+    
+    do
+    {
+        try game = PWBLevel(withName: name, startFrom: startFrom, endWith: goal, withOperations: ops)
+    } catch
+    {
+        print("Could not construct /(name)")
+    }
+    return game
+}
